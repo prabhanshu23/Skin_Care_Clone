8f73e12eacaa21db1ff6c0c72a9705497fd18171
Prathamesh52-28 <pnimje272@gmail.com>
2022-10-14 00:26:01 +0530

    Final

---
+++ Product1.js
@@ -0,0 +1,308 @@
+var Product1 = [
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/12932548-2534984326440526.jpg",
+        name: "SkinCeuticals Phyto Corrective Essence Mist 50ml",
+        box: "SkinCeuticals Gift",
+        price: "67.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11289609-8744892770898772.jpg",
+        name: "SkinCeuticals C E Ferulic with 15% L-Ascorbic Acid Vitamin C Serum 30ml",
+        box: "SkinCeuticals Gift",
+        price: "169.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11290635-9174978287699645.jpg",
+        name: "SkinCeuticals Triple Lipid Restore 2:4:2",
+        box: "SkinCeuticals Gift",
+        price: "136.00",
+
+    }, {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11289630-1894892772098685.jpg",
+        name: "SkinCeuticals Phyto+ 30ml",
+        box: "SkinCeuticals Gift",
+        price: "87.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11289619-1854892771554485.jpg",
+        name: "SkinCeuticals Hydrating B5 Hyaluronic Acid Gel Moisturizer 30ml",
+        box: "SkinCeuticals Gift",
+        price: "86.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11705713-2284962240533307.jpg",
+        name: "SkinCeuticals Hyaluronic Acid Intensifier Hydrating Serum 1 fl. oz",
+        box: "SkinCeuticals Gift",
+        price: "106.00",
+
+    },
+     {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11289601-7074972872341358.jpg",
+        name: "SkinCeuticals Anti-aging Skin Care Routine (Worth $481.00)",
+        box: "SkinCeuticals Gift",
+        price: "345.00",
+
+    }, {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11289629-4544892771975352.jpg",
+        name: "SkinCeuticals Phyto Corrective Gel 30ml",
+        box: "SkinCeuticals Gift",
+        price: "70.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11294635-1994892768429109.jpg",
+        name: "SkinCeuticals A.G.E. Interrupter",
+        box: "SkinCeuticals Gift",
+        price: "168.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11289599-4344892770055931.jpg",
+        name: "SkinCeuticals A.G.E. Eye Complex for Dark Circles 15g",
+        box: "SkinCeuticals Gift",
+        price: "105.00",
+
+
+        
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/13516546-2844930071318699.jpg",
+        name: "SkinCeuticals Phyto A+ Brightening Treatment 1 fl. oz",
+        box: "SkinCeuticals Gift",
+        price: "105.00",
+
+    }, {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11289626-1934892771763618.jpg",
+        name: "SkinCeuticals Phloretin CF with Ferulic Acid Vitamin C Serum 30ml",
+        box: "SkinCeuticals Gift",
+        price: "169.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11289606-5314892770614708.jpg",
+        name: "SkinCeuticals Blemish and Age Defense Serum",
+        box: "SkinCeuticals Gift",
+        price: "96.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11535233-1444892768930832.jpg",
+        name: "SkinCeuticals Purifying Cleanser 6.8 fl. oz",
+        box: "SkinCeuticals Gift",
+        price: "36.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11289635-1424892772796680.jpg",
+        name: "SkinCeuticals Resveratrol B E",
+        box: "SkinCeuticals Gift",
+        price: "156.00",
+
+    },
+     {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11582506-6064978637869764.jpg",
+        name: "SkinCeuticals Plump and Glow Regimen",
+        box: "SkinCeuticals Gift",
+        price: "268.00",
+
+    }, {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/12309426-1464892769568764.jpg",
+        name: "SkinCeuticals Glycolic Renewal Cleanser 5 fl.oz",
+        box: "SkinCeuticals Gift",
+        price: "39.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11679046-1554892769100800.jpg",
+        name: "SkinCeuticals Discoloration Defense Dark Spot Serum 30ml",
+        box: "SkinCeuticals Gift",
+        price: "102.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11289638-5054870995605268.jpg",
+        name: "SkinCeuticals Retinol 1.0",
+        box: "SkinCeuticals Gift",
+        price: "92.00",
+
+    },
+
+     {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/12771530-3164918929282606.jpg",
+        name: "SkinCeuticals Silymarin CF Serum 1 fl. oz",
+        box: "SkinCeuticals Gift",
+        price: "169.00",
+
+    }, 
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11289613-8694892771126034.jpg",
+        name: "SkinCeuticals Daily Moisture",
+        box: "SkinCeuticals Gift",
+        price: "66.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11888501-1494870996205118.jpg",
+        name: "SkinCeuticals Retinol 0.3 Refining Night Treatment 30ml",
+        box: "SkinCeuticals Gift",
+        price: "70.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11289636-3864892784609648.jpg",
+        name: "SkinCeuticals Retexturing Activator",
+        box: "SkinCeuticals Gift",
+        price: "86.00",
+
+    },
+    {
+        image_url: "https://static.thcdn.com/images/xsmall/webp//productimg/original/11289623-1824892771693055.jpg",
+        name: "SkinCeuticals Metacell Renewal B3",
+        box: "SkinCeuticals Gift",
+        price: "120.00",
+
+    },
+
+];
+reLoadM(Product1);
+
+function reLoadM(Product1){
+
+// DATA
+
+ displayDatas(Product1);
+function displayDatas(Product1){
+    document.querySelector("#container").textContent="";
+    Product1.map(function (data) {
+        var div = document.createElement("div")
+        document.querySelector("#container").append(div)
+    
+        // inside div creating image and  image_url
+        var image = document.createElement("img");
+        image.setAttribute("src", data.image_url);
+        div.append(image);
+    
+    
+        var name = document.createElement("p");
+        name.style.height = "35px"
+        name.textContent = data.name;
+        name.style.marginTop = "3%"
+        div.append(name);
+    
+        var box = document.createElement("div")
+        box.textContent = data.box
+        box.setAttribute("id", "boxer")
+        div.append(box)
+    
+    
+        var p2 = document.createElement("p")
+        p2.textContent = `$${data.price}`;
+        p2.style.marginTop = "3%"
+        p2.style.fontSize = "24px"
+        p2.style.fontWeight = "bold"
+        div.append(p2)
+    
+    
+    
+        var btn1 = document.createElement("button")
+        btn1.setAttribute("id", "btnwish")
+        btn1.textContent = "Wish"
+        btn1.style.display = "block"
+        btn1.addEventListener("click", function () {
+            wishlistAdd(data);
+        })
+        div.append(btn1)
+    
+    
+        var btn2 = document.createElement("button")
+        btn2.setAttribute("id", "btnbuy");
+        btn2.textContent = "QUICK BUY"
+        btn2.addEventListener("click", function () {
+            cartAdd(data);
+        })
+    
+        div.append(btn2);
+
+     
+    })
+    
+}
+
+
+var cartThings = JSON.parse(localStorage.getItem("cartthings")) || [];
+var cartArray = JSON.parse(localStorage.getItem("quickbuy1")) || [];
+function wishlistAdd(data) {
+    cartArray.push(data);
+    cartThings.push(data);
+    localStorage.setItem("quickbuy1", JSON.stringify(cartArray));
+    localStorage.setItem("cartthings", JSON.stringify(cartThings));
+    alert("Item Added");
+    reLoadM(robinData);
+
+}
+
+
+function cartAdd(data) {
+  
+    cartThings.push(data);
+  
+    localStorage.setItem("cartthings", JSON.stringify(cartThings));
+    alert("added to cart");
+    reLoadM(Product1);
+
+}
+
+var cartLength =JSON.parse(localStorage.getItem("cartthings"))
+document.querySelector("#cardAval").innerHTML=`There are currently ${cartLength.length} items in your cart.`;
+document.querySelector("#cartLen").innerHTML=`${cartLength.length}`;
+
+}
+
+
+function navSlideOut(){
+    document.querySelector("#nav-link").style.left="-16px"
+}
+function navSlideIn(){
+   document.querySelector("#nav-link").style.left="-2000px"
+}
+
+
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+
+function Tsort(){
+    var valT=document.querySelector("#sortT").value
+    console.log(valT)
+    if(valT==""){
+        reLoadM(Product1);
+    }else if(valT=="high"){
+        Product1.sort(function(a,b){
+         return (Number(b.price)-Number(a.price))
+     })
+     reLoadM(Product1);
+     
+    }else{
+        Product1.sort(function(a,b){
+            return (Number(a.price)-Number(b.price))
+     })
+     reLoadM(Product1);
+    }
+ }  
